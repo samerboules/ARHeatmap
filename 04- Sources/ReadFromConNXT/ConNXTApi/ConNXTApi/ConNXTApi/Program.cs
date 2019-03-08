b47f198fee42949df396837ec308e8e8ae53e558
SamerBoules <boulessamer@gmail.com>
2019-03-08 10:32:31 +0100

    Add a struct to collect the date to Samuel's Application to retrieve data from ConNXT

--- 04- Sources/ReadFromConNXT/ConNXTApi/ConNXTApi/ConNXTApi/Program.cs
+++ 04- Sources/ReadFromConNXT/ConNXTApi/ConNXTApi/ConNXTApi/Program.cs
@@ -3,10 +3,27 @@ using System;
 
 namespace ConNXTApi
 {
+
+    // Defining structure 
+    struct RuuviTag
+    {
+        // Declaring different data types 
+        public string _deviceID;
+        public string _timeStamp;
+        public string _temperature;
+        public string _humidity;
+        public string _pressure;
+        public string _accelerationX;
+        public string _accelerationY;
+        public string _accelerationZ;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            
             using (var api = new ConNXTApiConnector("https://beta.connxt.eu", "52840b9f-23db-475d-a920-272217be4402", "ZTXMI5Em/676HmLL0WUQUQ=="))
             {
                 api.ConnectAsync().Wait();
@@ -19,11 +36,21 @@ namespace ConNXTApi
                     Console.WriteLine($"{devices.Count} devices found.");
                     Console.WriteLine();
 
+                    //Create array of RuuviTags by the number of devices found
+                    RuuviTag [] Ruuvis = new RuuviTag[devices.Count];
+
+                    //local index to fill the structure of devices. Note that device number 0 is the gateway (RaspberryPi)
+                    int localindex = 0;
+
                     // Loop over all devices
                     foreach (JObject device in devices)
-                    {
+                    {                    
                         string deviceId = device["deviceUid"].Value<string>();
                         Console.WriteLine($"Telemetry for device: {deviceId}");
+
+                        
+                        Ruuvis[localindex]._deviceID = deviceId;
+                        Console.WriteLine($"_deviceID: {Ruuvis[localindex]._deviceID}");
 
                         // Retrieve the telemetry for the device
                         string telemetryString = api.GetTelemetryAsync(deviceId).Result;
@@ -32,12 +59,43 @@ namespace ConNXTApi
                             // Loop over the telemetry values
                             JObject telemetry = JObject.Parse(telemetryString);
                             Console.WriteLine($"{"TimeStamp",-15} = {telemetry["messageTimeStamp"]}");
+
+                            //Update timestamp in structure
+                            Ruuvis[localindex]._timeStamp = telemetry["messageTimeStamp"].ToString();
+
                             foreach (JObject dataPoint in telemetry["dataPoints"] as JArray)
                             {
                                 Console.WriteLine($"{dataPoint["key"],-15} = {dataPoint["value"]}");
+
+                                //Fill the structure with the updated data
+                                if (dataPoint["key"].ToString() == "AccelerationX")
+                                {
+                                    Ruuvis[localindex]._accelerationX = dataPoint["value"].ToString();
+                                }
+                                else if (dataPoint["key"].ToString() == "AccelerationY")
+                                {
+                                    Ruuvis[localindex]._accelerationY = dataPoint["value"].ToString();
+                                }
+                                else if (dataPoint["key"].ToString() == "AccelerationZ")
+                                {
+                                    Ruuvis[localindex]._accelerationZ = dataPoint["value"].ToString();
+                                }
+                                else if (dataPoint["key"].ToString() == "Temperature")
+                                {
+                                    Ruuvis[localindex]._temperature = dataPoint["value"].ToString();
+                                }
+                                else if (dataPoint["key"].ToString() == "Pressure")
+                                {
+                                    Ruuvis[localindex]._pressure = dataPoint["value"].ToString();
+                                }
+                                else if (dataPoint["key"].ToString() == "Humidity")
+                                {
+                                    Ruuvis[localindex]._humidity = dataPoint["value"].ToString();
+                                }
+
                             }
                         }
-
+                        localindex++;
                         Console.WriteLine();
                     }
                 }
